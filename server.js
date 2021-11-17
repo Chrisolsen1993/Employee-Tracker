d6b3bd1ad29bfbd69b9a6edba0f1c4050af97a8a
Olsen <chrisolsen1993@gmail.com>
2021-11-17 08:48:44 -0500

    view employee complete

--- server.js
+++ server.js
@@ -79,8 +79,13 @@ function viewAllEmployees(){
     db.query(`SELECT employee.id,
                      employee.first_name,
                      employee.last_name,
-                     role.title FROM employee JOIN role ON employee.role_id = role.id
-                     LEFT JOIN department ON role.department_id = department.id;`,
+                     role.title,
+                     department.name AS department,
+                     role.salary,
+                     CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                    FROM employee JOIN role ON employee.role_id = role.id
+                     LEFT JOIN department ON role.department_id = department.id
+                     LEFT JOIN employee manager ON employee.manager_id = manager.id;`,
     function(err, res){
    if (err) throw err
   
